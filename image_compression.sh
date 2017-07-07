896d1ad447ad3a57dfdb9ebae1a7aac2934fcd9d
Oskar Borek <oborek@pega-sus.de>
2017-07-07 18:25:14 +0200

    Update image_compression.sh

--- image_compression.sh
+++ image_compression.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ### CAUTION: Not use in Production
+# The Value of jpegoptim quality is verified. Need to test what's working fine for you
 
 // optimize jpegs recursivelly
 find . -type f -name "*.jpg" -exec jpegoptim -m50 --strip-all {} \;
