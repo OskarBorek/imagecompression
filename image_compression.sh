f73b22b584d5b15f09b5c42e3a7ace098ac15dbc
Oskar Borek <oborek@pega-sus.de>
2017-07-07 18:00:24 +0200

    Update image_compression.sh

--- image_compression.sh
+++ image_compression.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+### CAUTION: Not use in Production
 
 // optimize jpegs recursivelly
-find . -type f -name "*.jpg" -exec jpegoptim --strip-all {} \;
+find . -type f -name "*.jpg" -exec jpegoptim -m50 --strip-all {} \;
